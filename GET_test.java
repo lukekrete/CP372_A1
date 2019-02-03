39d5c8c6751c02a7e02d7f9e5ce092ed870b5354
lukekrete <lukekrete@gmail.com>
2019-02-03 17:25:57 -0500

    Start of the get function
    Correctly splits the info we've entered and assigns correct values to color, contain, message

--- GET_test.java
+++ GET_test.java
@@ -1,12 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.String;
+
+
+public class test {
+
+	public static void main(String[] args) {
+
+		String line = " color= red contains= 2,2 refersTo= the man";
+		//Open our scanner for string parsing
 		Scanner s = new Scanner(line);
-		String ignore = s.next();
-		String color = s.next();
-		ignore = s.next();
-		String contains = s.next();
-		ignore = s.next();
+		//initialize variables
+		String ignore = "";
+		String color = "";
+		String contains = "";
 		String message = "";
-		while(s.hasNext()){
-			message = message + " " + s.next();
+		//GETTING THE CORRECT INPUT------------------------------------------------------
+		if(line.contains("color= ")){
+			ignore = s.next();
+			color = s.next();
+			if(line.contains("contains= ")){
+				ignore = s.next();
+				contains = s.next();
+				if(line.contains("refersTo= ")){
+					ignore = s.next();
+					while(s.hasNext()){
+						message = message + " " + s.next();
+						message = message.trim();
+					}
+				}else{
+					message = null;
+				}
+			}else{
+				contains = null;
+				if(line.contains("refersTo= ")){
+					ignore = s.next();
+					while(s.hasNext()){
+						message = message + " " + s.next();
+						message = message.trim();
+					}
+				}else{
+					message = null;
+				}
+			}
+		}else{
+			color = null;
+			if(line.contains("contains= ")){
+				ignore = s.next();
+				contains = s.next();
+				if(line.contains("refersTo= ")){
+					ignore = s.next();
+					while(s.hasNext()){
+						message = message + " " + s.next();
+						message = message.trim();
+					}
+				}else{
+					message = null;
+				}
+			}else{
+				contains = null;
+				if(line.contains("refersTo= ")){
+					ignore = s.next();
+					while(s.hasNext()){
+						message = message + " " + s.next();
+						message = message.trim();
+					}
+				}else{
+					message = null;
+				}
+			}
 		}
-		message = message.trim();
+		//------------------------------------------------------------------------------------------
 		System.out.println("Color:" + color + " | Contains:" + contains + " | message:" + message);
+	}
+
+}
